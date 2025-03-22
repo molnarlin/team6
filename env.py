35921f4a16c495424168675e49d0515d878702f8
Katalin Molnar <molnarlin@gmail.com>
2025-03-22 21:33:46 +0000

    styling added to profile sites

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+os.environ["DB_NAME"] = "rippley_db"
+os.environ["DB_USER"] = "rippley_admin"
+os.environ["DB_PASSWORD"] = "t1000opnRPL"
+os.environ["DB_HOST"] = "212.227.155.64"
+os.environ["DB_PORT"] = "5432"
+os.environ["CLOUDINARY_URL"] = "cloudinary://815311621756144:Qywnd06cngz3HWqj2yaQ7wbiBDs@db6j7vxz1"
